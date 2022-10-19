500a9e65eda551e5290c73898426a258e98767dd
Ko Min Jae <54296776+kokojae@users.noreply.github.com>
2022-10-19 14:36:13 +0900

    파일 탐색 기능 추가
    출처: https://nauco.tistory.com/5

--- DeleteAndRename/main.cpp
+++ DeleteAndRename/main.cpp
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <iostream>
+#include <io.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	string path = "C:\\Users\\JYP\\Desktop\\p\\resource\\spam\\*.*";
+	struct _finddata_t fd;
+	intptr_t handle;
 
+	if ((handle = _findfirst(path.c_str(), &fd)) == -1L)
+		cout << "No file in directory!" << endl;
+
+	do {
+		cout << fd.name << endl;
+	} while (_findnext(handle, &fd) == 0);
+	_findclose(handle);
 
 	return 0;
 }
